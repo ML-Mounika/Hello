e3796abc0e285f6d65d9aad8a438be1ecf578ca7
ML-Mounika <159158311+ML-Mounika@users.noreply.github.com>
2025-09-24 20:18:11 +0530

    Update outputs.tf

--- Main/Main1/outputs.tf
+++ Main/Main1/outputs.tf
@@ -1,14 +1,14 @@
 output "records_for_flexdeploy" {
-  value = jsonencode([
+  value = base64encode(jsonencode([
     {
       action = "create-update"
       name   = "example-test.com"
       type   = "CNAME"
       value  = "www.example.com"
     }
-  ])
+  ]))
 }
-
+ 
 output "simple_message" {
   value = "HelloFlexDeploy"
 }
